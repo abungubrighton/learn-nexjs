72961354926b841a57b535701e8632399203d53f
abungubrighton <brighton@ibuqacapital.com>
2023-08-23 22:41:50 +0300

    chore: add routes, ammend layout(fonts), add header

---
+++ app/about/team/page.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title:"Brighton Hoods Company Team",
+    keywords:"Brighton Hoods Company Team , we bring all the good out of your company",
+}
+const AboutTeamPage = () => {
+  return (
+    <div>AboutTeamPage</div>
+  )
+}
+
+export default AboutTeamPage
